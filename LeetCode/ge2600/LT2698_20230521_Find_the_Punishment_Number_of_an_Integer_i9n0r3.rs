b211e6ea22f3ffe11a62f2d80a424032d8415dcb
wb_ <f12.628@hotmail.com>
2023-05-21 16:04:16 +0800

    2023-5-21

--- LeetCode/ge2600/LT2698_20230521_Find_the_Punishment_Number_of_an_Integer_i9n0r3.rs
+++ LeetCode/ge2600/LT2698_20230521_Find_the_Punishment_Number_of_an_Integer_i9n0r3.rs
@@ -4,8 +4,52 @@
 
 
 
+// Runtime20 ms
+// Beats
+// 100%
+// Sorry, there are not enough accepted submissions to show data
+// Memory1.9 MB
+// Beats
+// 100%
 
+impl Solution {
+    pub fn punishment_number(n: i32) -> i32 {
+        let mut ans: i32 = 0;
 
+        for i in 1..=n {
+            if Self::dfsa1(i, i * i) {
+                ans += i * i;
+            }
+        }
+
+        ans
+    }
+
+    fn dfsa1(rem: i32, ii: i32) -> bool {
+        if rem < 0 {
+            return false;
+        }
+        if rem == 0 {
+            if ii == 0 {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if ii == 0 {
+            return false;
+        }
+        let mut t2 = 1;
+        while ii >= t2 {
+            t2 *= 10;
+            if Self::dfsa1(rem - ii % (t2), ii / t2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
 
 
 
@@ -16,7 +60,7 @@ fn main()
 {
 
 
-    println!("ans: {:?}", Solution::());
+    println!("ans: {:?}", Solution::punishment_number(10));
 }
 
 
